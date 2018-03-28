fa4d7e9d5760427b0c1b70161a925f36418b3daf
Gokul C S <imgokulcs@gmail.com>
2018-03-28 23:23:50 +0530

    list layout implemented, roughly ,need to add layout implementation to lists

--- app/src/main/java/com/example/user/new2803/MainActivity.java
+++ app/src/main/java/com/example/user/new2803/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.user.new2803;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
             "WebOS","Ubuntu",
             "Windows7","Max OS X"};
 
+    ListView listView;
+    ArrayList<String> listitems = new ArrayList<String>();
+
+    ArrayAdapter<String> adapterty;
+
+
 
 
     @Override
@@ -23,23 +35,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        ArrayAdapter adapterli = new ArrayAdapter<String>(this,
-                R.layout.activity_main, mobileArray);
-        ListView listView = (ListView) findViewById(R.id.ListView);
-        listView.setAdapter(adapterli);
+        //list view adapter
+        listView=(ListView) findViewById(R.id.ListView);
+        adapterty = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mobileArray);
+        listView.setAdapter(adapterty);
 
 
-        */
 
 
+        //grid view adapter
         GridView g = (GridView) findViewById(R.id.grid_view);
         CustomAdapter adapter = new CustomAdapter(this, getData());
         g.setAdapter(adapter);
 
-
+        g.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        //
 
     }
+
 
     public ArrayList<Integer> getData() {
         ArrayList<Integer> list = new ArrayList<Integer>();
